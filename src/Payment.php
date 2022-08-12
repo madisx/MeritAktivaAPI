77a95e3c4aac7cbcf27f064e015a841679bc257a
Tormi Talv <tormi.talv@sportlyzer.com>
2022-08-12 17:32:55 +0300

    Payment data object update - amount fix - added all fields specified in api

--- src/Payment.php
+++ src/Payment.php
@@ -1,39 +1,52 @@
 <?php
 
 namespace Infira\MeritAktiva;
-class Payment extends \Infira\MeritAktiva\InvoiceGeneral
+
+/**
+ * https://api.merit.ee/connecting-robots/reference-manual/payments/create-payment/
+ */
+class Payment extends \Infira\MeritAktiva\General
 {
 	public function __construct()
 	{
-		$this->setMandatoryField('IBAN');
 		$this->setMandatoryField('CustomerName');
 		$this->setMandatoryField('InvoiceNo');
 		$this->setMandatoryField('Amount');
 	}
-	
+
+	public function setBankId(string $bankId)
+	{
+		$this->set('BankId', $bankId);
+	}
+
 	public function setIBAN(string $IBAN)
 	{
 		$this->set('IBAN', $IBAN);
 	}
-	
+
 	public function setCustomerName(string $CustomerName)
 	{
 		$this->set('CustomerName', $CustomerName);
 	}
-	
+
 	public function setInvoiceNo(string $InvoiceNo)
 	{
 		$this->set('InvoiceNo', $InvoiceNo);
 	}
-	
-	public function setRegNo(string $RegNo)
+
+	public function setPaymentDate(string $date)
 	{
-		$this->set('RegNo', $RegNo);
+        $this->set("PaymentDate", $this->convertDate($date));
 	}
-	
+
+	public function setRefNo(int $refNo)
+	{
+		$this->set('RefNo', $refNo);
+	}
+
 	public function setAmount(float $Amount)
 	{
-		$this->set('Amount', $this->toFloat());
+		$this->set('Amount', $Amount);
 	}
-	
+
 }
