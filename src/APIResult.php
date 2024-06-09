cc8edc764ea688bf9cfc1347e9d6defa29887353
Kaupo Einmann <kaupo93@gmail.com>
2024-06-09 15:29:38 +0300

    fix indentation

--- src/APIResult.php
+++ src/APIResult.php
@@ -12,7 +12,7 @@ class APIResult
 	
 	public function isError()
 	{
-		// sendinvoicebyemail method returns NULL on successful request
+	// sendinvoicebyemail method returns NULL on successful request
         // TODO I think its time to start checking status code as well
         if (is_null($this->res)) {
             return false;
